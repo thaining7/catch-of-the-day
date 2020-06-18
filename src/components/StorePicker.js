d1d7fb7b90e069e67e6e30410e008598654a07a4
Trevor <thaining@protonmail.com>
2020-06-18 17:35:36 -0500

    created custom event handler in store picker component

--- src/components/StorePicker.js
+++ src/components/StorePicker.js
@@ -2,13 +2,24 @@ import React from 'react';
 import { getFunName } from '../helpers';
 
 class StorePicker extends React.Component {
+
+    myInput = React.createRef();
+
+    goToStore = event => {
+        // 1. Stop the form from submitting on load
+        event.preventDefault();
+        // 2. Get the text from that input
+        console.log(this);
+        // 3. Change the page to /store/whatever-they-entered
+    }
+
     render() {
         console.log(this);
         return (
-            <form className="store-selector">
+            <form className='store-selector' onSubmit={this.goToStore}>
                 <h2>Please Enter A Store</h2>
-                <input type="text" required placeholder="Store Name" defaultValue={ getFunName() } />
-                <button type="submit">Visit Store</button>
+                <input type='text' ref={this.myInput} required placeholder='Store Name' defaultValue={ getFunName() } />
+                <button type='submit'>Visit Store</button>
             </form>
         )
     }
